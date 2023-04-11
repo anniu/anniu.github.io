f792abf87f87ffec45504cb7c440aa33064370be
Anni <anni.uutma@guardtime.com>
2023-04-11 11:57:14 +0300

    init

---
+++ main.js
@@ -0,0 +1,88 @@
+// https://github.com/fireship-io/threejs-scroll-animation-demo/blob/main/main.js
+
+import './style.css'
+import * as THREE from 'three';
+import {OrbitControls} from "three/addons/controls/OrbitControls.js";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({color: 0xFF00FF});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.position.set(20, 20, 20);
+scene.add(pointLight);
+
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshBasicMaterial({color: 0xffffff})
+    const star = new THREE.Mesh(geometry, material);
+
+    const [x, y, z] = Array(3).fill(null).map(() => THREE.MathUtils.randFloatSpread(100));
+
+    star.position.set(x, y, z);
+    scene.add(star);
+}
+
+Array(200).fill(null).forEach(addStar);
+
+// const spaceTexture = new THREE.TextureLoader().load('1433682.jpg');
+// scene.background = spaceTexture;
+
+const cowsTexture = new THREE.TextureLoader().load('cows-in-the-dark.jpg');
+
+const cows = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: cowsTexture }));
+
+scene.add(cows);
+
+cows.position.z = -15;
+cows.position.setX(-10);
+
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+/*    cows.rotation.x += 0.05;
+    cows.rotation.y += 0.075;
+    cows.rotation.z += 0.05;*/
+
+  //camera.position.z = t * -0.0001;
+  //camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
+
+    cows.rotation.x += 0.005;
+
+    // to move with mouse
+    // controls.update();
+
+    renderer.render(scene, camera);
+}
+
+animate();
